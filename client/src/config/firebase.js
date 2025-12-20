9097dc7116eaf00ff2df53eebd34c1ec76ebca29
Akanksha negi <ankiakanksha707@gmail.com>
2025-12-20 13:11:57 +0530

    hi

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -3,10 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth,GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAYZ77FDO5nQ-XGapuvHYsYhDrN5yvHsFs",
-    authDomain: "focuslab-90a57.firebaseapp.com",
-    projectId: "focuslab-90a57",
-    appId: "1:1005613284290:web:e0c0ed3e5e5d036e99745b",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
